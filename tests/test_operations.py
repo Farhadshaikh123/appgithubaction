b856347c559602dd53ff8749c10db9070cb98c70
Farhad <farhadshaikh11@hotmail.com>
2025-12-03 16:36:39 +0100

    updated unit tests for mult function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-from src.math_operations import add, sub
+from src.math_operations import add, sub, mult
 
 def test_add():
     assert add(2,3)==5
@@ -7,3 +7,6 @@ def test_add():
 def test_sub():
     assert sub(10,5)==5
     assert sub(20,10)==10
+
+def test_mult():
+    assert mult(10,10)==100
